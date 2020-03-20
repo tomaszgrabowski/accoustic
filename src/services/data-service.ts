98d16050878c43f9bffac48dd1a37c7d2a7633fc
Tomasz Grabowski <tomasz.grabowski@ihsmakit.com>
2020-03-20 16:12:07 +0100

    some minor fixes

--- src/services/data-service.ts
+++ src/services/data-service.ts
@@ -1,7 +1,7 @@
-import { DATA_FETCH_URL } from '../constants';
+import { DATA_FETCH_URL, DEFAULT_ARTICLE_ID } from '../constants';
 
-export const getData = async (url = DATA_FETCH_URL) => {
-    const response = await fetch( url );
+export const getArticleByGuid = async ( id = DEFAULT_ARTICLE_ID ) => {
+    const response = await fetch( `${ DATA_FETCH_URL }${ id }` );
     if ( !response.ok ) throw new Error( `Api responded with error: ${ response.statusText }` );
     const deserializedData = await response.json();
     return deserializedData;
